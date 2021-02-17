7a4b11ea1343848d95ccfc0b5ae21bc447f6fc3f
dkyryk <kyryk.dmytro@gmail.com>
2021-02-17 22:43:40 +0200

    add spring boot

---
+++ src/main/kotlin/com/ercarts/kotlin/theater/TheaterApplication.kt
@@ -0,0 +1,14 @@
+package com.ercarts.kotlin.theater
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+/**
+ * @author dkyryk
+ */
+@SpringBootApplication
+class TheaterApplication
+
+fun main(args: Array<String>) {
+    runApplication<TheaterApplication>(*args)
+}
---
+++ src/test/kotlin/com/ercarts/kotlin/theater/TestTheaterStartup.kt
@@ -0,0 +1,16 @@
+package com.ercarts.kotlin.theater
+
+/**
+ * @author dkyryk
+ */
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestTheaterStartup {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
